32f853b64ed1d6c7de245fa7abb56b2d56ef810b
Abhishek Kumar <94506000+AbhishekCS3459@users.noreply.github.com>
2023-08-15 16:54:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,25 +18,9 @@ const port = process.env.PORT || 4000;
 require("dotenv").config();
 app.use(express.json());
 
-const corsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      "http://localhost:5173",
-      "https://book-your-place-azure.vercel.app",
-      // Add more allowed origins here if needed
-    ];
-
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
 // // Use the CORS policy
-app.use(cors(corsOptions));
+app.use(require("cors"));
 
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
